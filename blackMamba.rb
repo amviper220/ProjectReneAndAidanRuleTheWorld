30ef41d165f680d48641e7830d0474af840016f8
sik <siknasty106@gmail.com>
2014-01-13 22:14:27 -0600

    first iteration of xml downloader
    We need to figure out if we want the directory where the xmls are stored
    to be there permanently or be deleted after they are run through the
    parser.  I also need to figure out a way to deduce which builds are new.
    This could be based either on time, or a value we have stored somewhere.

---
+++ blackMamba.rb
@@ -0,0 +1,16 @@
+#!/usr/bin/ruby
+
+Dir.mkdir("buildXmls")
+
+Dir.chdir("./buildXmls")
+$limit = 1046  #this needs to be a param read from a file or find out someway to get the last build number....possibly time?
+$buildnumber = 1043
+while $buildnumber < $limit
+	greeting = "grabbing infra build #$buildnumber"
+	puts greeting
+	url =  "http://ci-gsix.isg.deere.com/job/gsx-dev-infra-default/#$buildnumber/testReport/api/xml"
+	`wget #{url}` 
+	$buildnumber +=1
+end 
+
+exit 0
